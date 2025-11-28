ab1eff287383de644ec15edd1ce893b98c5fae25
Clulnes <art.yusupov2003@yandex.ru>
2025-11-28 21:45:14 +0300

    Финальный вариант проекта спринта 7

--- src/ru/yandex/practicum/WordleDictionary.java
+++ src/ru/yandex/practicum/WordleDictionary.java
@@ -1,6 +1,9 @@
 package ru.yandex.practicum;
 
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /*
 этот класс содержит в себе список слов List<String>
@@ -8,7 +11,36 @@ import java.util.List;
     также этот класс может содержать рутинные функции по сравнению слов, букв и т.д.
  */
 public class WordleDictionary {
+    private List<String> words = new ArrayList<>();
+    private PrintWriter log;
 
-    private List<String> words;
+    public WordleDictionary(PrintWriter log) {
+        this.log = log;
+    }
 
+    public void addWord(String word) {
+        words.add(word);
+    }
+
+    public String getRandomWord() throws DictionaryIsEmptyException {
+        if (words.isEmpty()) {
+            log.println("Ошибка: попытка взятия слова из пустого файла.");
+            throw new DictionaryIsEmptyException("Словарь пуст! Невозможно выбрать слово.");
+        }
+
+        Random random = new Random();
+        return words.get(random.nextInt(words.size()));
+    }
+
+    public int getCount() {
+        return words.size();
+    }
+
+    public boolean isContains(String word) {
+        return words.contains(word);
+    }
+
+    public List<String> getWords() {
+        return new ArrayList<>(words);
+    }
 }
